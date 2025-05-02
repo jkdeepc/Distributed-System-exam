74264997adbc6b7a0ce6010cba62a440f90f75cc
jkdeepc <20109320@mail.wit.ie>
2025-05-02 10:28:23 +0100

    EDA architecture

--- lambdas/lambdaX.ts
+++ lambdas/lambdaX.ts
@@ -1,10 +1,25 @@
+
+
 import { Handler } from "aws-lambda";
 
 export const handler: Handler = async (event, context) => {
   try {
+    
     console.log("Event: ", JSON.stringify(event));
 
+    
+    for (const record of event.Records) {
+      
+      const body = JSON.parse(record.body);
+      console.log("Lambda X received SQS record:", body);
+
+      
+    }
+
+    
   } catch (error: any) {
+    
+    console.error("Error in Lambda X:", error);
     throw new Error(JSON.stringify(error));
   }
 };
--- lambdas/lambdaY.ts
+++ lambdas/lambdaY.ts
@@ -1,11 +1,25 @@
+
+
 import { Handler } from "aws-lambda";
 
 export const handler: Handler = async (event, context) => {
   try {
+    
     console.log("Event: ", JSON.stringify(event));
 
-  } catch (error: any) {
-    throw new Error(JSON.stringify(error));
+    
+    for (const record of event.Records) {
+      
+      const message = record.Sns.Message;
+      console.log("Lambda Y received SNS message:", message);
 
+      
+    }
+
+    
+  } catch (error: any) {
+    console.error("Error in Lambda Y:", error);
+    
+    throw new Error(JSON.stringify(error));
   }
 };
